119dda7d3e010452bfbffc31bca9cdcd90074527
Mario-Recio <m.recio.2020@alumnos.urjc.es>
2022-10-13 13:44:16 +0200

    fix: new activity date and hour display, now work properly

--- app/src/main/java/com/devfiveurjc/agendaly/NewTaskActivity.java
+++ app/src/main/java/com/devfiveurjc/agendaly/NewTaskActivity.java
@@ -14,17 +14,27 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.Calendar;
+
 public class NewTaskActivity extends AppCompatActivity {
 
     private int[] date = new int[3];
     private int [] hour = new int [2];
     private TextView displayDate, displayHour;
+    MaterialTextView textView1,textView2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+        textView1 = (MaterialTextView) findViewById(R.id.textView4);
+        textView2= (MaterialTextView) findViewById(R.id.textView5);
+        textView1.setText("");
+        textView2.setText("");
+
     }
 
     public void switchMenuActivity(View view) {
@@ -36,7 +46,49 @@ public class NewTaskActivity extends AppCompatActivity {
         displayDate.setText(StringChange.dateString(date));
     }*/
 
-    public void openDate(View v) {
+    public void openDate(View view) {
+        final Calendar c= Calendar.getInstance();
+        date[0]=c.get(Calendar.DAY_OF_MONTH);
+        date[1]=c.get(Calendar.MONTH);
+        date[2]=c.get(Calendar.YEAR);
+        DatePickerDialog dialog = new DatePickerDialog(NewTaskActivity.this,
+                android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date[0]=dayOfMonth;
+                        date[1]=month;
+                        date[2]=year;
+                        textView1.setText(date[0]+"/"+(date[1]+1)+"/"+date[2]);
+                    }
+                },
+                date[2], date[1], date[0]);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    public void openHour(View view) {
+        final Calendar c= Calendar.getInstance();
+        hour[0]=c.get(Calendar.HOUR_OF_DAY);
+        hour[1]=c.get(Calendar.MINUTE);
+        ContextThemeWrapper newContext = new ContextThemeWrapper(this, R.style.Theme_Agendaly_Dialog);
+        TimePickerDialog tmd = new TimePickerDialog(newContext, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                hour[0]=hourOfDay;
+                hour[1]=minute;
+                if(hour[1]<10){
+                    textView2.setText(hour[0]+":0"+hour[1]);
+                }else {
+                    textView2.setText(hour[0] + ":" + hour[1]);
+                }
+
+            }
+        }, hour[0], hour[1], true);
+        tmd.show();
+    }
+/*
+    public void openDate(View view) {
         DatePickerDialog dialog = new DatePickerDialog(
                 NewTaskActivity.this,
                 android.R.style.Theme_Holo_Light_Dialog_MinWidth,
@@ -52,8 +104,10 @@ public class NewTaskActivity extends AppCompatActivity {
                 date[2], date[1], date[0]);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
+        MaterialTextView textView = findViewById(R.id.textView4);
     }
-
+    */
+    /*
     public void openHour(View view) {
         ContextThemeWrapper newContext = new ContextThemeWrapper(this, R.style.Theme_Agendaly_Dialog);
         TimePickerDialog tmd = new TimePickerDialog(newContext, new TimePickerDialog.OnTimeSetListener() {
@@ -66,5 +120,7 @@ public class NewTaskActivity extends AppCompatActivity {
         }, hour[0], hour[1], true);
         tmd.show();
     }
+
+     */
 
 }
